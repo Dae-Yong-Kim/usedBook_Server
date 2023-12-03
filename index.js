7d5124a7cb74976898bbc6d12e1cbcb95146b1eb
Dae-Yong-Kim <youbinlove607@gmail.com>
2023-12-04 00:44:49 +0900

    .

--- index.js
+++ index.js
@@ -14,23 +14,11 @@ console.log("test");
 io.on("connection", (socket) =>{
     socket["nickname"] = "Anon";
     socket.onAny((event) => console.log(`Socket Event: ${event}`));
-    socket.on("enter_room", (roomName, done) => {
+    socket.on("enter_room", (roomName) => {
         socket.join(roomName);
-        socket.to(roomName).emit("welcome", socket.nickname);
-        done();
     })
-    socket.on("offer", (offer, roomName) => {
-        socket.to(roomName).emit("offer", offer);
-    })
-    socket.on("answer", (answer, roomName) => {
-        socket.to(roomName).emit("answer", answer);
-    })
-    socket.on("ice", (ice, roomName) => {
-        socket.to(roomName).emit("ice", ice);
-    })
-    socket.on("new_message", (msg, room, done) => {
+    socket.on("new_message", (msg, room) => {
         socket.to(room).emit("new_message", socket.nickname, msg);
-        done();
     })
     socket.on("nickname", (nickname) => socket["nickname"] = nickname);
 });
